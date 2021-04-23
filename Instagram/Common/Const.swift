bac0797c8a1cd70ea298104e2726df265ed29f54
takatoshi.ichige <takatoshi.ichige>
2021-04-23 11:34:17 +0900

    投稿処理実装

---
+++ Instagram/Common/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by takatoshi.ichige on 2021/04/22.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
--- Instagram/ViewControllers/PostViewController.swift
+++ Instagram/ViewControllers/PostViewController.swift
@@ -6,25 +6,70 @@
 //
 
 import UIKit
+import Firebase
 
 class PostViewController: UIViewController {
 
     var image: UIImage!
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imageView.image = image
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func handlePostButton(_ sender: Any) {
+        // 画像の変換
+        let jpegImage = image.jpegData(compressionQuality: 0.75)!
+        // 画像と投稿のデータ格納先を定義
+        let postRef = Firestore.firestore().collection(Const.PostPath).document()
+        let imageRef = Storage.storage().reference().child(Const.ImagePath).child(postRef.documentID + "jpg")
+        
+        // HUD
+        HUD.show()
+        
+        
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpeg"
+        // Storageに画像データをアップする
+        imageRef.putData(jpegImage, metadata: metaData) { metadata, error in
+            if let error = error {
+                print(error)
+                HUD.showError(withStatus: "画像のアップロードでエラーが発生しました。")
+                
+                // 先頭の画面に戻る
+                UIApplication.shared.windows.first{ $0.isKeyWindow }?.rootViewController?.dismiss(animated: true, completion: nil)
+                return
+            }
+            
+            // FireStoreに投稿データを保存
+            // 保存用にデータをまとめておく
+            let name = Auth.auth().currentUser?.displayName
+            let postDoc = [
+                "name" : name!,
+                "caption" : self.titleTextField.text!,
+                "date" : FieldValue.serverTimestamp()
+            ] as [String : Any]
+            
+            postRef.setData(postDoc)
+            
+            HUD.showSuccess(withStatus: "投稿が完了しました。")
+            
+            // 先頭の画面に戻る
+            UIApplication.shared.windows.first{ $0.isKeyWindow }?.rootViewController?.dismiss(animated: true, completion: nil)
+            
+        }
+        
+        
     }
-    */
-
+    @IBAction func handleCancelButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
