6693ac7cb8af0d4be08c6406ee4da92dd480626b
takatoshi.ichige <takatoshi.ichige>
2021-04-27 10:55:15 +0900

    カスタムセルの作成

---
+++ Instagram/Views/PostTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostTableViewCell.swift
+//  Instagram
+//
+//  Created by takatoshi.ichige on 2021/04/27.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var likeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
