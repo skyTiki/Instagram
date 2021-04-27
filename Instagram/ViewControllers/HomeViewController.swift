27d44ad99476698a7d43038e943dc77b5e220f3d
takatoshi.ichige <takatoshi.ichige>
2021-04-27 11:29:03 +0900

    カスタムセルの作成（TableViewに登録まで）

--- Instagram/ViewControllers/HomeViewController.swift
+++ Instagram/ViewControllers/HomeViewController.swift
@@ -7,10 +7,33 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // 投稿データ
+    var postArray: [PostData] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // xib 登録
+        let nib = UINib(nibName: "PostTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "Cell")
+    }
+    
+    // MARK: - TableViewDelegate, DataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        postArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! PostTableViewCell
+        cell.setPostData(postArray[indexPath.row])
+        
+        return cell
     }
 }
--- Instagram/Views/PostTableViewCell.swift
+++ Instagram/Views/PostTableViewCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseUI
 
 class PostTableViewCell: UITableViewCell {
     
@@ -23,4 +24,36 @@ class PostTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
     }
     
+    func setPostData(_ postData: PostData) {
+        // 画像
+        // クラウドからダウンロードする（ダウンロード中はインジケーターを表示）
+        postImageView.sd_imageIndicator = SDWebImageActivityIndicator.gray
+        let imageRef = Storage.storage().reference().child(Const.ImagePath).child(postData.id + ".jpg")
+        postImageView.sd_setImage(with: imageRef)
+        
+        // 説明
+        captionLabel.text = "\(postData.name!) : \(postData.caption!)"
+        
+        // 日付
+        dateLabel.text = ""
+        if let date = postData.date {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd HH:mm"
+            let dateString = formatter.string(from: date)
+            self.dateLabel.text = dateString
+        }
+        
+        // いいね数
+        let likeNumber = postData.likes.count
+        likeLabel.text = "\(likeNumber)"
+        
+        // いいねボタン
+        if postData.isLiked {
+            let buttonImage = UIImage(named: "like_exist")
+            self.likeButton.setImage(buttonImage, for: .normal)
+        } else {
+            let buttonImage = UIImage(named: "like_none")
+            self.likeButton.setImage(buttonImage, for: .normal)
+        }
+    }
 }
